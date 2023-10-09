d2141e71a74dac28ca5ea98349c9a6f2131a1dc8
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2023-10-09 11:10:43 +0100

    Add files via upload

---
+++ LotteryApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using LotteryApp.concrete;
+using LotteryApp.Constants;
+
+Console.WriteLine(LotteryConstants.APPLICATION_NAME);
+
+IrishLotto irishLotto = new IrishLotto();
+
+List<int> numbers = irishLotto.GenerateLotteryNumbers();
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+Console.WriteLine();
---
+++ LotteryApp/concrete/IrishLotto.cs
@@ -0,0 +1,38 @@
+﻿using LotteryApp.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryApp.concrete
+{
+    public class IrishLotto : LotteryGenerator
+    {
+        List<int> lotto = new List<int>();
+        public override List<int> GenerateLotteryNumbers()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < LotteryConstants.TOTAL_LOTTERY_NUMBERS; i++)
+            {
+
+
+                int currentNumber = rnd.Next(1, LotteryConstants.MAX_RANGE);
+
+                while (lotto.Contains(currentNumber))
+                {
+                    currentNumber = rnd.Next(1, LotteryConstants.MAX_RANGE);
+                }
+
+                lotto.Add(currentNumber);
+
+
+
+            }
+
+            lotto.Sort();
+
+            return lotto;
+        }
+    }
+}
